cce94af891c78060c5416ce0220b0ee56ecfa384
Anik Datta <47003868+anik511@users.noreply.github.com>
2020-03-13 19:23:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function signout()
+{
+    
+    var signout = document.getElementById("signin");
+    signout.innerHTML = "Sign Out";
+    document.getElementById("close").style.display="none";
+    return true;
+    
+}
